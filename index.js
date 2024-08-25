37391e22737a6b797ed0e524188d5165baecb569
Adham Bahaa <63783446+AdhamBahaa@users.noreply.github.com>
2024-08-25 21:23:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ var score = prompt("Enter your Exam Score:");
 // if else Statements
 
 if (score < 100 && score >= 85) {
+  alert("You got an A 🎉");
   console.log("You got an A 🎉");
 } else if (score < 85 && score >= 75) {
-  console.log("You got a B 🎉");
+  alert("You got a B 🎉");
+  console.log("You got an B 🎉");
 } else if (score < 75 && score >= 65) {
+  alert("You got a C 📜");
   console.log("You got a C 📜");
 } else if (score < 65 && score >= 50) {
+  alert("You got a D 👌");
   console.log("You got a D 👌");
 } else if (score < 50 && score >= 0) {
+  alert("You got a F 😟");
   console.log("You got a F 😟");
 } else if (score > 100 || score < 0) {
+  alert("Invalid Score ✖️");
   console.log("Invalid Score ✖️");
 } else if (score == 100) {
+  alert("Perfect Score 🏆");
   console.log("Perfect Score 🏆");
 } else if (typeof score != Number) {
+  alert("Not a Number ✖️");
   console.log("Not a Number ✖️");
 }
 
@@ -24,26 +32,34 @@ if (score < 100 && score >= 85) {
 
 switch (true) {
   case score == 100:
+    alert("Perfect Score 🏆");
     console.log("Perfect Score 🏆");
     break;
   case score > 100 || score < 0:
+    alert("Invalid Score ✖️");
     console.log("Invalid Score ✖️");
     break;
   case score < 100 && score >= 85:
+    alert("You got an A 🎉");
     console.log("You got an A 🎉");
     break;
   case score < 85 && score > 75:
-    console.log("You got a B 🎉");
+    alert("You got a B 🎉");
+    console.log("You got an B 🎉");
     break;
   case score <= 75 && score > 65:
+    alert("You got a C 📜");
     console.log("You got a C 📜");
     break;
   case score <= 65 && score > 50:
+    alert("You got a D 👌");
     console.log("You got a D 👌");
     break;
   case score <= 50 && score > 0:
-    console.log("You got an F 😟");
+    alert("You got an F 😟");
+    console.log("You got a F 😟");
     break;
   default:
+    alert("Not a Number ✖️");
     console.log("Not a Number ✖️");
 }
